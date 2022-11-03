5b0beff4f1dee5421dc42a45100b2ef87695bce6
Oguzhan Ozdemir <aoozdemir@live.com>
2022-11-03 16:51:24 +0300

    test

---
+++ hello-world/jest.config.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+exports.__esModule = true;
+exports["default"] = {
+    transform: {
+        '^.+\\.ts?$': 'esbuild-jest'
+    },
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    testMatch: ['**/tests/unit/*.test.ts']
+};
